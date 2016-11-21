805db6b01fb1a36ffae626f1a3e1d7b6ad794d4d
Matti Petrelius <matti.petrelius@devisioona.fi>
2016-11-21 23:22:41 +0200

    Trying to get the app to work on Azure

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(80, function () {
+app.listen(process.env.PORT, function () {
     console.log('Example app listening on port 80!');
 });
